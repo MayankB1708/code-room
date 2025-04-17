2997cbf8e1883e869293a6ef7ddd2df91fa8932c
Mayank Bisht <mayankbisht17082004@gmail.com>
2025-04-18 01:56:35 +0530

    Project base configured: Convex DB, Clerk auth, Stream chat, and shadcn UI

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="m-10">Home Page</div>
+  )
+}
