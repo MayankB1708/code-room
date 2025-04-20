d35f6941b35f95df3503cc803b7dc2f66e2bbbf3
Mayank Bisht <mayankbisht17082004@gmail.com>
2025-04-20 23:39:59 +0530

    Created Navbar, added toggle button for light/dark mode and dashboard button for the interviewer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,8 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="m-10">
-      <SignInButton>
-        <Button>Login</Button>
-      </SignInButton>
+    <div className="m-10">Home Page
     </div>
   )
 }
+
